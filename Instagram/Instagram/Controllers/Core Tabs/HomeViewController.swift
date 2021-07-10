5d9643ae6408ae7bdd33fa00da3206c29a24d789
Ned Ememanka <officialchinedu@gmail.com>
2021-07-10 19:07:45 -0400

    First Commit

---
+++ Instagram/Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Ned Ememanka on 2021-07-09.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Instagram/Instagram/Controllers/Core Tabs/NotificationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationViewController.swift
+//  Instagram
+//
+//  Created by Ned Ememanka on 2021-07-10.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
